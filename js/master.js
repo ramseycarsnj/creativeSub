37fc83bc484b399969c1e75827c367ceb36ba397
Joe Romero <joethemovie@gmail.com>
2017-11-01 15:46:04 -0400

    first build of form submission site

---
+++ js/master.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  $('#mobileMenu').hide();
+  $('#loader').delay(100).fadeOut();
+
+  $('#inactive').click(function(){
+    $('nav').css('height', '200px');
+    $('#mobileMenu').delay(100).fadeIn();
+    $('#inactive').hide();
+    $('#active').show();
+    console.log('I work');
+  });
+  $('#active').click(function(){
+    $('nav').css('height', '70px');
+    $('#mobileMenu').hide();
+    $('#active').hide();
+    $('#inactive').show();
+    console.log('I work');
+  });
+
+  $('.videoEvent').hide();
+  $('.videoCom').hide();
+
+  $('#yes').click(function(){
+    $('#question').hide();
+    $('.videoEvent').fadeIn();
+    console.log('I work');
+  });
+
+  $('#no').click(function(){
+    $('#question').hide();
+    $('.videoCom').fadeIn();
+    console.log('I work');
+  });
+
+
+  $('.freebirdFormviewerViewFormBanner').css('display', 'none');
+});
